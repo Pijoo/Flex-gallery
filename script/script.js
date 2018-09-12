9d0796d2215ad4e625d045dffbeb02a7b4ae86f5
Pijoo <bartosz.surzyn@gmail.com>
2018-09-12 23:31:04 +0200

    flex gallery

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+const allPanels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+	this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+	if(e.propertyName.includes('flex')) {
+		this.classList.toggle('open-active');
+	}
+}
+
+allPanels.forEach(panel => panel.addEventListener('click', toggleOpen));
+allPanels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
